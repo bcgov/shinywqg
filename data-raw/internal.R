65fed129d140c239d8e621d2bcde5c579e9598ef
Seb Dalgarno <seb@poissonconsulting.ca>
2020-02-12 12:06:20 -0800

    add draft version of new wqg data

--- data-raw/internal.R
+++ data-raw/internal.R
@@ -1,12 +1,11 @@
-limits <-  read.csv(system.file(package = "shinywqg", "extdata/limits.csv"), stringsAsFactors = FALSE)
+limits <-  read.csv(system.file(package = "shinywqg", "extdata/all_wqgs.csv"), stringsAsFactors = FALSE)
 limits <- dplyr::mutate(limits, Condition = dplyr::if_else(Condition == "", NA_character_, Condition))
-limits2 <- limits[1:20,] 
-limits2$Use <- "Salty Times"
-limits <- rbind(limits, limits2)
-codes <-  read.csv(system.file(package = "shinywqg", "extdata/codes.csv"), stringsAsFactors = FALSE)
+
+codes <- limits %>% dplyr::select(Variable, EMS_Code, Units) %>% dplyr::distinct()
+# codes <-  read.csv(system.file(package = "shinywqg", "extdata/codes.csv"), stringsAsFactors = FALSE)
 
 missing_help <- "There are two reasons why guideline values may be missing:
                 1. A condition was not met;
                 2. There is no available equation for that variable/use/term combination."
 
-usethis::use_data(limits, limits2, codes, missing_help, internal = TRUE, overwrite = TRUE)
+usethis::use_data(limits, codes, missing_help, internal = TRUE, overwrite = TRUE)
