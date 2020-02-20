42b2ee938b50768d52b7de1a04ea4aec3587702a
Seb Dalgarno <seb@poissonconsulting.ca>
2020-02-20 12:32:47 -0800

    fix paths

--- R/mod_data.R
+++ R/mod_data.R
@@ -254,7 +254,7 @@ mod_data_server <- function(input, output, session) {
   output$dl_html <- downloadHandler(
     filename = "wqg_report.html",
     content = function(file) {
-      path <- file.path(rv$path, "report_html.Rmd")
+      path <- "inst/extdata/report_html.Rmd"
       temp_report <- file.path(tempdir(), "report_html.Rmd")
       file.copy(path, temp_report, overwrite = TRUE)
       
@@ -275,7 +275,7 @@ mod_data_server <- function(input, output, session) {
   output$dl_pdf <- downloadHandler(
     filename = "wqg_report.pdf",
     content = function(file) {
-      path <- file.path(rv$path, "report_pdf.Rmd")
+      path <- "inst/extdata/report_pdf.Rmd"
       temp_report <- file.path(tempdir(), "report_pdf.Rmd")
       file.copy(path, temp_report, overwrite = TRUE)
       
@@ -297,7 +297,7 @@ mod_data_server <- function(input, output, session) {
   output$dl_rmd <- downloadHandler(
     filename = "wqg_report.Rmd",
     content = function(file) {
-      file.copy(file.path(rv$path, "report_html.Rmd"), file)
+      file.copy("inst/extdata/report_html.Rmd", file)
     }
   )
 }
