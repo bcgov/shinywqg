5e8551cb9f35fd7d430044624772d6be59c29e94
Seb Dalgarno <seb@poissonconsulting.ca>
2020-02-20 13:06:54 -0800

    test path

--- R/mod_data.R
+++ R/mod_data.R
@@ -275,7 +275,7 @@ mod_data_server <- function(input, output, session) {
   output$dl_pdf <- downloadHandler(
     filename = "wqg_report.pdf",
     content = function(file) {
-      path <- "inst/extdata/report_pdf.Rmd"
+      path <- "ins/extdata/report_pdf.Rmd"
       temp_report <- file.path(tempdir(), "report_pdf.Rmd")
       file.copy(path, temp_report, overwrite = TRUE)
       
