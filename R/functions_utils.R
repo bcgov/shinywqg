1ab778ed51b60887c355699a9289cb05492384a1
Seb Dalgarno <seb@poissonconsulting.ca>
2020-03-11 11:50:50 -0700

    remove blank

--- R/functions_utils.R
+++ R/functions_utils.R
@@ -10,6 +10,7 @@ get_combinations <- function(variable, component, use, data = limits) {
   l
 }
 
+
 extract_codes <- function(x) {
   setdiff(unique(unlist(lapply(x, function(y){
     stringr::str_extract_all(y, "EMS_[[:alnum:]][[:alnum:]_]{3,3}")
