6e5a1633364ccc4e151834564c6f9f8fbff397eb
Seb Dalgarno <seb@poissonconsulting.ca>
2020-03-24 13:11:23 -0700

    add processing steps, make Notes NA if missing

--- R/functions_data.R
+++ R/functions_data.R
@@ -1,13 +1,31 @@
-process_limits <- function(x){
-  x <- x %>%
+process_limits <- function(limits){
+  limits <- limits %>%
     dplyr::mutate(Condition = dplyr::if_else(Condition == "", NA_character_, Condition))
   
   ### remove Hardness Dissolved in OR context
-  modified <- x$Condition[which(stringr::str_detect(x$Condition, "EMS_0107"))] %>%
+  modified <- limits$Condition[which(stringr::str_detect(limits$Condition, "EMS_0107"))] %>%
     stringr::str_split_fixed("\\|", 2)
   modified <- modified[, 1]
-  x$Condition[which(stringr::str_detect(x$Condition, "EMS_0107"))] <- modified
-  x
+  limits$Condition[which(stringr::str_detect(limits$Condition, "EMS_0107"))] <- modified
+  
+  #### start convert background percent to limit notes #####
+  limits <- mutate(limits, PC = !is.na(Limit) & str_detect(Limit, paste0(
+    "^(\\s*", EMS_Code, "\\s*[*]\\s*\\d[.]\\d+\\s*$)|",
+    "(^\\s*\\d[.]\\d+\\s*[*]\\s*",EMS_Code, "\\s*$)")))
+  
+  limits$Limit[limits$PC] %<>% str_replace("EMS_[[:alnum:]_]{4,4}", "100")
+  
+  limits$Limit[limits$PC] %<>% sapply(function(x) eval(parse(text=x)))
+  
+  limit_notes <- str_c(limits$Limit[limits$PC], "% background")
+  limits$Limit[limits$PC] <- NA_character_
+  
+  limit_notes %<>% str_c(if_else(is.na(limits$LimitNotes[limits$PC]), ".",
+                                 str_c(" (", limits$LimitNotes[limits$PC], ").")))
+  
+  limits$LimitNotes[limits$PC] <- limit_notes
+  limits <- select(limits, -PC)
+  limits
 }
 
 wqg_filter <- function(variable, use, media, x = limits) {
@@ -44,14 +62,16 @@ wqg_clean <- function(data, sigfig) {
     format_guideline(data$Guideline[x],
                      data$Direction[x],
                      data$Units[x],
+                     data$LimitNotes[x],
                      sigfig)
   })
   
   data %>%
-    dplyr::mutate(Notes = gsub("NA", "", paste(LimitNotes, MethodNotes))) %>%
+    dplyr::mutate(Notes = gsub("NA", "", paste(ConditionNotes, MethodNotes))) %>%
+    dplyr::mutate(Notes = dplyr::if_else(Notes == " ", NA_character_, Notes)) %>%
     dplyr::select(Variable, Component, Value = Use, Media, Type, 
       `Predicted Effect Level` = PredictedEffectLevel,
-      Status, `Condition Notes` = ConditionNotes,
+      Status, 
       `WQG Narrative` = NarrativeWQG, Notes,
       Guideline, Reference:`Technical Document Link`)
 }
