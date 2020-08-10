d27226c3c4f0590da4ed6258e8f6a539c7bb9872
Seb Dalgarno <seb@poissonconsulting.ca>
2020-08-10 14:03:14 -0700

    improve check function, dont export modules

--- R/check_guidelines.R
+++ R/check_guidelines.R
@@ -2,10 +2,16 @@
 #'
 #' Check that updated DataBC guidelines are valid
 #' 
-#' @param x A data.frame of the guidelines.
+#' @param x A data.frame of the guidelines. IF NULL, pull from BC Data Catalogue.
 #' @return A invisible copy of the data.frame of the guidelines.
 #' @export
-check_guidelines <- function(x){
+check_guidelines <- function(x = NULL){
+  
+  if(is.null(x)){
+    message("reading data from BC Data Catalogue ...")
+    x <-  bcdata::bcdc_get_data(record = "85d3990a-ec0a-4436-8ebd-150de3ba0747")
+    x <- process_limits(x)
+  }
   
   chk::check_names(x, names(limits))
   
@@ -20,19 +26,19 @@ check_guidelines <- function(x){
   names(cvalues) <- cvalue_codes
   cvalues <- lapply(cvalues, function(x) x <- 1)
   
-
-  check_valid_expression <- function(x, cvalues) {
+  check_valid_expression <- function(x, cvalues, colname) {
     for(i in 1:length(x)){
       limit <- x[i]
       if(is.na(limit))
         next
       y <- try(eval(parse(text = limit), envir = cvalues), silent = TRUE)
       if(is_try_error(y))
-        stop("Expression ", i, " is not valid")
+        stop(paste0("The value in row ", i, " of column '", colname, 
+                   "' is not a valid R expression. Please fix."))
     }}
 
-  check_valid_expression(x$Condition, cvalues)
-  check_valid_expression(x$Limit, cvalues)
+  check_valid_expression(x$Condition, cvalues, "Condition")
+  check_valid_expression(x$Limit, cvalues, "Limit")
   
   x
 }
