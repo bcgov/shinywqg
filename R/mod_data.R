86a5d218c731c22b267b3f53d10a5c224932e4f8
Ayla Pearson <aylapearson23@gmail.com>
2020-12-13 13:52:33 -0800

    do not show sig fig for lookup values

--- R/mod_data.R
+++ R/mod_data.R
@@ -99,6 +99,9 @@ mod_data_server <- function(input, output, session) {
     x <- wqg_data_evaluate()
     x <- x[x$ConditionPass,]
     if(any(is.na(suppressWarnings(as.numeric(x$Limit)))) & all(!is.na(x$Limit))){
+      if(any(str_detect(x$Limit, "\\.csv$"))) {
+        return()
+      }
       return(
         tagList(
           tags$label("Guideline Significant Figures"),
