2f197c18fcc4fd9b992ff2934be12bca928b9ad3
Ayla Pearson <aylapearson23@gmail.com>
2021-03-17 16:03:55 -0700

    clean up

--- app.R
+++ app.R
@@ -1,11 +1,4 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 source('global.R', local = TRUE)
 
 ui <- function() {
@@ -30,11 +23,8 @@ ui <- function() {
 }
 
 
-
 server <- function(input, output, session) {
-  
   callModule(mod_data_server, "data_ui_1")
-  
 }
 
 # Run the application 
