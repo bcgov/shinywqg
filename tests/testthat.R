77a4b0ab7e04b3b56b21da6989eb78328a4f733b
Ayla Pearson <aylapearson23@gmail.com>
2021-03-21 11:39:55 -0700

    updating testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,5 +2,13 @@
 # run this script then you can run each script in testthat
 
 library(testthat)
+library(magrittr)
 
 load("R/sysdata.rda", envir=.GlobalEnv)
+source("R/check_guidelines.R")
+source("R/functions_data.R")
+source("R/functions_limits.R")
+source("R/functions_output.R")
+source("R/functions_utils.R")
+source("R/utils.R")
+source("R/sys.R")
