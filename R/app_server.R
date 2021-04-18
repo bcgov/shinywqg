c20387cf8406e4c622da39ba69696f7b76d4d23c
Ayla Pearson <aylapearson23@gmail.com>
2021-04-18 11:05:16 -0700

    adding base app functions back

---
+++ R/app_server.R
@@ -0,0 +1,3 @@
+app_server <- function(input, output, session) {
+  callModule(mod_data_server, "data_ui_1")
+}
---
+++ R/run_app.R
@@ -0,0 +1,9 @@
+#' Run Shiny Application
+#'
+#' @export
+run_wqg_app <- function() {
+  
+  shiny::shinyAppDir(system.file("app", package = "shinywqg"),
+                     options = c("launch.browser" = TRUE))
+  
+}
