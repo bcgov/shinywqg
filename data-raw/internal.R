d404582f63a86a804515961e532f33e60b402ec1
Andy Teucher <andy.teucher@gmail.com>
2024-03-26 12:28:36 -0700

    Back to getting internal data from updated data in catalogue
    * remove limits csv file
    * don't need to rename cu lookup columns since updated in BCDC
    * Rebuild internal data

--- data-raw/internal.R
+++ data-raw/internal.R
@@ -8,15 +8,10 @@ hash_limits <- "85d3990a-ec0a-4436-8ebd-150de3ba0747"
 hash_cu_acute <- "23ada5c3-67a6-4703-9369-c8d690b092e1"
 hash_cu_chronic <- "a35c7d13-76dd-4c23-aab8-7b32b0310e2f"
 
-## TODO: Temporary workaround until CU lookup tables updated in BCDC
-## Use local file (the same one as testing within mod_data_server()) to 
-## generate internal limits and codes if it hasn't been updated in the 
-## catalogue yet
-# limits <-  bcdata::bcdc_get_data(
-#   record = hash_limits,
-#   resource = "6f32a85b-a3d9-44c3-9a14-15175eba25b6"
-# )
-limits <- read_csv("inst/app/all-wqgs-2.csv")
+limits <-  bcdata::bcdc_get_data(
+  record = hash_limits,
+  resource = "6f32a85b-a3d9-44c3-9a14-15175eba25b6"
+)
 
 codes <- read_csv("data-raw/codes.csv")
 
@@ -46,8 +41,6 @@ lookup_hash <- c(hash_cu_chronic, hash_cu_acute)
 
 for (file in lookup_hash) {
   lookup <- bcdata::bcdc_get_data(record = file)
-  # TODO: Temporary workaround until CU lookup tables updated in BCDC
-  lookup <- rename(lookup, any_of(c("EMS_1103" = "EMS_1126")))
   internal_tables[[paste0(file)]] <- lookup
 }
 
